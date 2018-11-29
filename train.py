f7e4057c491990d6297abf0c7b8a39e4fe892d72
Pengcheng Rong <rp32374@cs.utexas.edu>
2018-11-29 13:14:18 -0600

    validation

--- train.py
+++ train.py
@@ -24,25 +24,52 @@ def gpu(obj):
 		return obj.cuda()
 	else:
 		return obj
-'''
-def hack_loss(states, labels):
-	total_loss = 0
+iterations_per_period = 300
+calling_frequency = 10
+prev_period_accuracy = 1
+total_accuracy = .0
+degrading_periods = 0
+model_of_last_period = {}
+trainning_model = os.path.join(dirname, 'model.training.th')
 
-	for i in range(states.size()[0]):
-		for j in range(states.size()[1]):
-			if states[i][j][6] < -0.5:
-				if labels[i][j][4] == 0:
-					total_loss += 1
-				if labels[i][j][5] == 1:
-					total_loss += 1
-			elif states[i][j][6] > 0.5:
-				if labels[i][j][4] == 1:
-					total_loss += 1
-				if labels[i][j][5] == 0:
-					total_loss += 1
+def auto_early_stopping(iteration, accuracy, model):
+	#accuracy is actually validation loss. Smaller means better
+	global total_accuracy
+	global prev_period_accuracy
+	global model_of_last_period
+	global degrading_periods
+	global trainning_model
+	accuracy_plateaued = False
+	if iteration !=0 and iteration % iterations_per_period == 0:
+		period_accuracy = total_accuracy / (iterations_per_period / calling_frequency) # cause we call this function every calling_frequency iterations
+		if period_accuracy > prev_period_accuracy:
+			degrading_periods = degrading_periods + 1
+			accuracy_plateaued = True
+			print('Warning! Validation loss %f'%period_accuracy, ' is higher than all-time low %f'%prev_period_accuracy)
+			if degrading_periods > 3:
+				print('Recommend early stopping because the performance is worse than all-time low for 4 consective periods.')
+				return True, accuracy_plateaued, model_of_last_period
+		else:
+			degrading_periods = 0
+			model_of_last_period = model
+			trained_model = {}
+			if torch.cuda.is_available():
+				cpu_model_dict = {}
+				for x in model_of_last_period:
+					cpu_model_dict[x] = model_of_last_period[x].cpu()
+				trained_model = cpu_model_dict
+			else:
+				trained_model = model_of_last_period
 
-	return torch.tensor(total_loss / states.numel())
-'''
+			torch.save(trained_model, trainning_model)
+			print("Validation loss %f"%period_accuracy, " is lower than previously seen low %f"%prev_period_accuracy)
+			if prev_period_accuracy < 1 and (prev_period_accuracy - period_accuracy) / prev_period_accuracy < 0.01:
+				#accuracy rate increased less than 1 percent during this period
+				accuracy_plateaued = True
+			prev_period_accuracy = period_accuracy
+		total_accuracy = .0		
+	total_accuracy = total_accuracy + accuracy
+	return False, accuracy_plateaued, None
 
 def train(max_iter, batch_size=1, log_dir=None, aggre = None):
 	'''
@@ -57,10 +84,10 @@ def train(max_iter, batch_size=1, log_dir=None, aggre = None):
 		train_dataloader = load('train', num_workers=0, batch_size=batch_size, subset = aggre)
 	else:
 		train_dataloader = load('train', num_workers=0, batch_size=batch_size)
-	#valid_dataloader = load('val', num_workers=0, crop=40, batch_size=batch_size)
+	valid_dataloader = load('val', num_workers=0, crop=128, batch_size=batch_size)
 
 	train_dataloader_iterator = cycle(train_dataloader)
-	#valid_dataloader_iterator = cycle(valid_dataloader)
+	valid_dataloader_iterator = cycle(valid_dataloader)
 
 	model = gpu(Model())
 	print (model)
@@ -120,10 +147,29 @@ def train(max_iter, batch_size=1, log_dir=None, aggre = None):
 
 	
 		if t % 10 == 0 and t > 0:	
-			print('[%5d]  t_loss = %f'%(t, t_loss_val))
+			batch_obs, batch_states, batch_actions = next(valid_dataloader_iterator)
+			batch_obs = gpu(batch_obs.float().permute(0,1,4,2,3))
+			batch_states = gpu(batch_states)
+			batch_actions = gpu(batch_actions)
+			
+			model_outputs = model(batch_obs)
+			
+			v_loss_val = loss(model_outputs, batch_actions)
+
+			print('[%5d]  t_loss = %f   v_loss = %f'%(t, t_loss_val,v_loss_val))
 			if log is not None:
 				log.add_scalar('train/loss', t_loss_val, t)
-				#log.add_scalar('val/loss', v_loss_val, t)
+				log.add_scalar('val/loss', v_loss_val, t)
+
+			early_stop, accuracy_plateaued, last_good_model = auto_early_stopping(t, v_loss_val, model.state_dict())
+			if (early_stop == True):
+				model.load_state_dict(last_good_model)
+				break
+			elif accuracy_plateaued == True and learning_rate > 1e-4:
+				learning_rate /= 2.0
+				for param_group in optimizer.param_groups:
+					param_group['lr'] = learning_rate
+				print('learning rate dropped to %f'%learning_rate)
 			torch.save(model.state_dict(), trainning_model) 
 
 	torch.save(model.state_dict(), os.path.join(dirname, 'model.th')) # Do NOT modify this line
