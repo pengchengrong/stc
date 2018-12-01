ecfe720fd64de8c5aa0130d1d80d0e810f4e6322
Pengcheng Rong <rp32374@cs.utexas.edu>
2018-12-01 15:36:46 -0600

    james model

--- train.py
+++ train.py
@@ -114,7 +114,7 @@ def train(max_iter, batch_size=1, log_dir=None, aggre = None):
 	optimizer = optim.Adam(model.parameters(), lr = learning_rate, weight_decay=1e-4)
 	
 	train_class_loss_weights = np.array([
-		1., 10., 10., 1., 3., 3.
+		1., 20., 10., 1., 3., 3.
 	])
 	# Loss criterion. Your need to replace this with one that considers class imbalance
 	sig = nn.Sigmoid()
