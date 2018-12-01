0a4813eec5ea58bde5b0e150755b6400496b6571
Pengcheng Rong <pengtsen@utexas.edu>
2018-12-01 00:25:23 -0600

    image normal

--- train.py
+++ train.py
@@ -140,11 +140,11 @@ def train(max_iter, batch_size=1, log_dir=None, aggre = None):
 		t_loss_val = loss(predicted_actions, batch_actions)
 		
 		if t % 50 == 0 and t > 0:
-			np.set_printoptions(precision=2, suppress = True)
+			torch.set_printoptions(precision=2)
 			print("train prediction:")
-			print (predicted_actions[:, 0:5, :].detach().numpy())
+			print (predicted_actions)
 			print("train labels:")
-			print (batch_actions[:, 0:5].detach().numpy())
+			print (batch_actions)
 
 		# Compute the gradient
 		t_loss_val.backward()
@@ -164,11 +164,11 @@ def train(max_iter, batch_size=1, log_dir=None, aggre = None):
 			v_loss_val = loss(predicted_actions, batch_actions)
 
 			if t % 50 == 0 and t > 0:
-				np.set_printoptions(precision=2, suppress = True)
+				torch.set_printoptions(precision=2)
 				print("val prediction:")
-				print (predicted_actions[:, 0:5, :].detach().numpy())
+				print (predicted_actions)
 				print("val labels:")
-				print (batch_actions[:, 0:5].detach().numpy())
+				print (batch_actions)
 
 			print('[%5d]  t_loss = %f   v_loss = %f'%(t, t_loss_val,v_loss_val))
 			if log is not None:
